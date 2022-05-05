d7e88dc42ec9fea29b372a801293a076778ce857
Aqil Zeeshan <azeeshan@freewheel.com>
2022-05-05 23:42:30 +0100

    Signup/Signin

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -2,9 +2,28 @@ import Link from 'next/link'
 import React from 'react'
 import '../../configureAmplify'
 import {useState, useEffect} from 'react'
+import {Auth, Hub} from 'aws-amplify'
 
 const Navbar = () => {
-    //const [signedUser, setSignedUser] = useState(false)
+    const [signedUser, setSignedUser] = useState(false)
+    useEffect(()=>{
+        authListener();
+    },[])
+
+    async function authListener(){
+        Hub.listen("auth",(data)=>{
+            switch(data.payload.event){
+                case "signIn":
+                    return setSignedUser(true)
+                case "signOut":
+                    return setSignedUser(false)
+            }
+        })
+        try{
+            await Auth.currentAuthenticatedUser()
+            setSignedUser(true)
+        }catch(err){}
+    }
 
     return (
         <nav className='flex justify-center pt-3 pb-3 space-x-4 border-b bg-cyan-500 border-gray-300'>
@@ -18,6 +37,14 @@ const Navbar = () => {
                         <a className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">{title}</a>
                     </Link>
                 ))
+            }
+
+            {
+                signedUser && (
+                    <Link href='/my-posts'>
+                        <a className='rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900'>My Post</a>
+                    </Link>
+                )
             }
         </nav>
     )
